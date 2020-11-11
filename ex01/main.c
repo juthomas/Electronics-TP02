b78944afa385686d8ea20482ae8d62a7d9e897cf
The Drunken Wizard <johangourdin94@gmail.com>
2020-11-11 23:33:32 +0100

    sf

--- ex01/main.c
+++ ex01/main.c
@@ -5,10 +5,13 @@
 #include <avr/interrupt.h>
 #include <avr/io.h>
 
-volatile uint32_t timerCount = 0;
+volatile uint32_t timerCount = 1;
 ISR(TIMER0_COMPA_vect)
 {
-        ++timerCount;
+		timerCount++;
+		if(timerCount > 100)
+			timerCount = 1;
+        OCR1A = 10;
 }
 
 int main(void)
@@ -17,17 +20,20 @@ int main(void)
         TIMSK0 |= 1 << OCIE0A;
         TCCR0A |= 1 << WGM00 | 1 << WGM01;
         TCCR0B |= 1 << WGM02;
-        OCR0A = 124;
+        OCR0A = 6250;
         //sei();
 		SREG|=(1<<7);
         TCCR0B |= 1 << CS02 | 1 << CS00;
+		// On met la frequence PWM sur 1Hz: 16MHz/(256 * 1Hz) = 62500
+		ICR1 = 62500;
+		// On met le duty-cycle sur 10%: 62500 * 0.1 = 6250
+		OCR1A = 6250;
+		// On active le mode PWM sur OC1A (16.11.1)
+		TCCR1A = (1 << COM1A1) | (1 << WGM11);
+		// On active fast PWM et le prescaler sur 256 pour le timer 1 (16.11.2)
+		TCCR1B = (1 << WGM13) | (1 << WGM12) | (1 << CS12);
         while(1)
         {
-        	if(timerCount >= 125)
-            {
-                    PORTB=~PORTB;
-                        timerCount = 0;
-        	}
         }
         return 0;
 }
